22d095020e92617c7eca02f06abd4957d8b460e4
Yannick Aaron Lehr <yannick.lehr@ioki.com>
2022-01-17 11:41:47 +0100

    new changes

---
+++ src/lib/calculator.py
@@ -0,0 +1,31 @@
+"""
+Test module with calculations
+"""
+
+
+def add(a: int, b: int) -> int:
+    """
+    Add two numbers
+    """
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """
+    Subtract two numbers
+    """
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """
+    Multiply two numbers
+    """
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """
+    Divide two numbers
+    """
+    return a / b
